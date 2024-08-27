eccc9b3b3250d4be4122cfd1fd065aa756928740
Aegizz <lloyddraysey@gmail.com>
2024-08-27 17:11:56 +0930

    Test

--- memsim.c
+++ memsim.c
@@ -104,8 +104,7 @@ int     allocateFrame( int page_number)
 					//update values
 					table->entries[i].pageNo = page_number;
 					table->entries->modified = 0;
-					removeIndex(table->q, i);
-					enqueue(table->q,i);
+
 					//return iterator to it
 					return i;
 				}
